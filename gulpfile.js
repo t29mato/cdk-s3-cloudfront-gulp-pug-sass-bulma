b25884d786b53e904b24f7134f32e0cadc3ce722
Tomoya Mato <t.mato@R0052.local>
2020-01-13 15:36:03 +0900

    GulpにPugを組み込む

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,34 @@
-// gulpプラグインの読み込み
 const gulp = require("gulp");
-// Sassをコンパイルするプラグインの読み込み
 const sass = require("gulp-sass");
+const pug = require('gulp-pug');
+const del = require('del');
 
-// style.scssをタスクを作成する
-gulp.task("default", function() {
-  // style.scssファイルを取得
-  return (
-    gulp
-      .src(["src/site-contents/scss/main.scss", "node_modules/bulma/bulma.sass"])
-      // Sassのコンパイルを実行
-      .pipe(sass())
-      // cssフォルダー以下に保存
-      .pipe(gulp.dest("build/site-contents/css"))
-  );
-});
+const paths = {
+  src: 'src/site-contents',
+  dest: 'build/site-contents'
+}
+
+// SCSS
+function css (cb) {
+  gulp.src([paths.src + "/scss/main.scss", "node_modules/bulma/bulma.sass"])
+    .pipe(sass())
+    .pipe(gulp.dest(paths.dest + "/css"));
+  console.log('css');
+  cb();
+}
+
+// Pug
+function html (cb) {
+  gulp.src(paths.src + '/pug/*')
+  .pipe(pug({pretty: true}))
+  .pipe(gulp.dest(paths.dest + "/"))
+  cb();
+}
+
+// Clean
+function clean (cb) {
+  del(paths.dest + '/*');
+  cb();
+}
+
+exports.default = gulp.series(clean, gulp.parallel(css, html));
